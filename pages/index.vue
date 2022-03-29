2f83f395c642ebf07faf473ffcaefb1d457b284d
ducnguyen <ntanduc94@gmail.com>
2022-03-29 19:34:17 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="submitHandler">
+        <input id="itemInput" v-model="item" type="text">
+        <button @click="setTodoList">Add</button>
+        <button>Search</button>
+    </form>
+    <div v-for="(tditem, index) in todoList">
+        {{tditem}} <button @click="removeCurrItem(index)">Remove</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    const item = useState<String>('item', () => '')
+    const todoList = useState<String[]>('todoList', () => ['1','2'])
+
+    const setTodoList = () => {
+        todoList.value = [...todoList.value, item.value]
+        item.value = ''
+        document.getElementById('itemInput').focus()
+    }
+    const removeCurrItem = (index: number) => {
+        todoList.value.splice(index,1)
+    }
+    const submitHandler = (data: any) => {
+        console.log(data)
+    }
+</script>
