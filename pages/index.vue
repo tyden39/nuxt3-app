7fae628f4f024d56af31ebc6d0fb894b43e10a55
ducnguyen <ntanduc94@gmail.com>
2022-03-31 09:44:42 +0700

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -10,18 +10,25 @@
 </template>
 
 <script lang="ts" setup>
-    const item = useState<String>('item', () => '')
-    const todoList = useState<String[]>('todoList', () => ['1','2'])
+    const item = useState<string>('item', () => '')
+    const todoList = useState<string[]>('todoList', () => ['1','2'])
     const todoItem = ref()
+    const originList = useState<string[]>('originList', () => todoList.value)
+
+    const searchList = () => {
+        todoList.value = todoItem.value.filter
+    }
 
     const setTodoList = () => {
         todoList.value = [...todoList.value, item.value]
         item.value = ''
         todoItem.value.focus()
     }
+
     const removeCurrItem = (index: number) => {
         todoList.value.splice(index,1)
     }
+
     const submitHandler = (data: any) => {
         console.log(data)
     }
